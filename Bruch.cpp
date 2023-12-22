587e30a31dc97513ad8c36afa58a761c123ae18b
zoisk <zoisk64@gmail.com>
2023-12-22 01:57:32 +0100

    u

---
+++ Bruch.cpp
@@ -0,0 +1,31 @@
+//
+// Created by zoisk on 22.12.23.
+//
+
+#include "Bruch.h"
+
+namespace bruch {
+    int Bruch::kuerzen() {
+        return 0;
+    }
+
+    int Bruch::ggt() {
+        return 0;
+    }
+
+    Bruch::Bruch(int n, int z) {
+
+    }
+
+    int Bruch::Zähler() {
+        return 0;
+    }
+
+    int Bruch::Nenner() {
+        return 0;
+    }
+
+    double Bruch::reell() {
+        return 0;
+    }
+} // bruch
---
+++ Bruch.h
@@ -0,0 +1,25 @@
+//
+// Created by zoisk on 22.12.23.
+//
+
+#ifndef U13_BRUCH_H
+#define U13_BRUCH_H
+
+namespace bruch {
+
+    class Bruch {
+    private:
+        int n;
+        int z;
+        int kuerzen();
+        int ggt();
+    public:
+        Bruch(int n, int z);
+        int Zähler();
+        int Nenner();
+        double reell();
+    };
+
+} // bruch
+
+#endif //U13_BRUCH_H
